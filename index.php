a42d45380e9162e933212fb4ccb195359d7df13b
EnzaEnzi <enza.webdev@gmail.com>
2020-12-07 16:16:13 +0100

    Mise en place

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Name file :   Index
+ * Description : The main template file
+ *
+ * @package WordPress
+ * @subpackage avimayeur
+ * @version 1.0
+ */
+?>
+
+<?php get_header() ?>
+<section>
+    <h1>Hello Word</h1>
+</section>
+<?php get_footer() ?>
+
